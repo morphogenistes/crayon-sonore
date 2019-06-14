4d2e0b083cb9bbceedc4be9b3af6eef65b9b3292
josephlarralde <joseph.larralde@gmail.com>
2019-06-15 01:56:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Crayon sonore
+
+### Projet Arduino permettant de réaliser un circuit électronique de dessin sonore.
+
+Matériel requis :
+
+* une carte arduino
+* un crayon en graphite
+* un haut-parleur piezo
+* une résistance 10k
+* une plaque de prototypage
+* du fil électronique
+* un câble croco
---
+++ crayon-sonore.ino
@@ -0,0 +1,48 @@
+// #define SERIAL_DEBUG
+
+const int capt = A0;
+const int piezo = 9;
+
+float stylo = 0;
+const int maxStylo = 1000;
+
+// MIDI notes (69 <=> 440 Hz)
+float ambitus = 66;
+float offset = 42;
+
+void setup() {
+  pinMode(piezo,OUTPUT);
+  pinMode(A0,INPUT);
+  
+#ifdef SERIAL_DEBUG
+  Serial.begin(9600);
+#endif
+}
+
+void loop() {
+  stylo = analogRead(capt);
+  
+#ifdef SERIAL_DEBUG
+  Serial.println(stylo);
+#endif
+
+  if (stylo <= maxStylo) {  
+    // convertit l'entrée analog in en MIDI (normalisation puis conversion)
+    
+    // Normalisation et mise à l'échelle :
+    stylo = pow(stylo / (float) maxStylo, 1.f) * ambitus + offset;
+  
+    // Conversion MIDI en frequence
+    // stylo = (exp(0.057762265) * stylo * 69) / 440;
+    // ou :
+    // Fonction carré pour privilégier les fréquences graves
+    // stylo = 440.f * pow(2.f, (stylo - 69.f) / 12.f);
+    // caster stylo en int pour un rendu chromatique 
+    stylo = 440.f * pow(2.f, ((int) stylo - 69.f) / 12.f);
+    
+    tone(piezo, stylo);
+  } else {
+    //coupe si trop loin
+    noTone(piezo);
+  }
+}
